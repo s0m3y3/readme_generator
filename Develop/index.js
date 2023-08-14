b7e77cfbf2586f7f296096d9b7a1c86a0020662e
Yee Y <s0m3y3@gmail.com>
2023-08-14 18:54:22 -0500

    +node_modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,13 @@
 // TODO: Include packages needed for this application
+//Installed "npm install inquirer"
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
